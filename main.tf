2094bb3550c4e97e6fe1806d5896ad85ae82b1c4
zoki <tosevzoran@gmail.com>
2025-01-02 17:19:09 +0100

    feat(domain): add domain for droplet (#3)

--- main.tf
+++ main.tf
@@ -7,3 +7,21 @@ resource "digitalocean_droplet" "self" {
   region = each.value.region
   size   = each.value.size
 }
+
+resource "godaddy-dns_record" "self" {
+  for_each = { for record_key, record in flatten([
+    for droplet_key, droplet in local.droplets : [
+      for record in droplet.domain.records : {
+        name   = record.name
+        type   = record.type
+        domain = record.domain
+        data   = digitalocean_droplet.self[droplet_key].ipv4_address
+      }
+    ]
+  ]) : record_key => record }
+
+  name   = each.value.name
+  type   = each.value.type
+  domain = each.value.domain
+  data   = each.value.data
+}
