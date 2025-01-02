db3568942014fb71fff368722e05c74f2704eed6
zoki <tosevzoran@gmail.com>
2025-01-02 17:17:55 +0100

    feat(domain): add domain for droplet

--- locals.tf
+++ locals.tf
@@ -4,6 +4,16 @@ locals {
       image  = "ubuntu-24-10-x64"
       region = "fra1"
       size   = "s-1vcpu-512mb-10gb"
+
+      domain = {
+        records = [
+          {
+            domain = "image.sandboxs.me"
+            type   = "A"
+            name   = "@"
+          }
+        ]
+      }
     }
   }
 }
--- versions.tf
+++ versions.tf
@@ -2,6 +2,11 @@ terraform {
   required_version = "~> 1.10.3"
 
   required_providers {
+    godaddy-dns = {
+      source  = "registry.terraform.io/veksh/godaddy-dns"
+      version = "~> 0.3.9"
+    }
+
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
