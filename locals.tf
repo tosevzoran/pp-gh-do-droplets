59ae606f034a0c76c35329d39254355cd920b9d5
zoki <tosevzoran@gmail.com>
2025-11-01 14:03:32 +0100

    chore(*): delete droplet (#4)
    Co-authored-by: Zoran Tošev <zoran.tosev@symphony.is>

--- locals.tf
+++ locals.tf
@@ -1,24 +1,24 @@
 locals {
   droplets = {
-    "basic-web" = {
-      image  = "ubuntu-24-10-x64"
-      region = "fra1"
-      size   = "s-1vcpu-512mb-10gb"
-
-      domain = {
-        records = [
-          {
-            domain = "astro-blog.sandboxs.me"
-            type   = "A"
-            name   = "@"
-          },
-          {
-            domain = "ciao.sandboxs.me"
-            type   = "A"
-            name   = "@"
-          }
-        ]
-      }
-    }
+    # "basic-web" = {
+    #   image  = "ubuntu-24-10-x64"
+    #   region = "fra1"
+    #   size   = "s-1vcpu-512mb-10gb"
+    #
+    #   domain = {
+    #     records = [
+    #       {
+    #         domain = "astro-blog.sandboxs.me"
+    #         type   = "A"
+    #         name   = "@"
+    #       },
+    #       {
+    #         domain = "ciao.sandboxs.me"
+    #         type   = "A"
+    #         name   = "@"
+    #       }
+    #     ]
+    #   }
+    # }
   }
 }
